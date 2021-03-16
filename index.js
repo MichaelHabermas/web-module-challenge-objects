add957e3bc9777a8263519613ec6bc9c6a76c562
MichaelHabermas <michaelghabermas@gmail.com>
2021-03-16 08:31:01 -0700

    Donzo

--- index.js
+++ index.js
@@ -96,19 +96,29 @@ const reviews = [
 Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
   */
-// console.log(reviews.name['Julius'].feedback);
+console.log(reviews[4].feedback);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Using the reviews array above do the following: (no function needed) 
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
+reviews.push({
+	name: 'Michael',
+	rating: 4.0,
+	feedback:
+		'Attentive service, excellent cuisine! But the decor leaves something to be desired. I give this place...2 stars. Unsubscribe.'
+});
+console.log(reviews);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Reyna's feedback is missing! Use what you know to do the following: (no function needed) 
   1. Add this feedback to Reyna's rating - "this place is chill with really cool people, great for getting work done on weekdays"
   2. log the reviews array to the console to check your work
 */
+
+reviews[7].feedback = 'this place is chill with really cool people, great for getting work done on weekdays';
+console.log(reviews);
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Write a function to return a review based on the index of the review in the array.
